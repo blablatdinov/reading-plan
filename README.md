ef36a7cc6132fc7e0dccab699aa51ae110ff8d46
Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
2023-05-13 01:57:33 +0300

    Initial

---
+++ README.md
@@ -0,0 +1,26 @@
+# Program for calculation reading plan
+
+Enter start, end pages and dates
+
+```
+Enter start page: 
+691
+Enter end page: 
+767
+Enter start date: 
+2023-03-27
+Enter end date: 
+2023-04-02
+```
+
+You take next output:
+
+```
+Date: 2023-03-27. Pages: 691-701
+Date: 2023-03-28. Pages: 702-712
+Date: 2023-03-29. Pages: 713-723
+Date: 2023-03-30. Pages: 724-734
+Date: 2023-03-31. Pages: 735-745
+Date: 2023-04-01. Pages: 746-756
+Date: 2023-04-02. Pages: 757-767
+```
---
+++ src/main.rs
@@ -0,0 +1,118 @@
+use std::{io, process};
+use chrono::*;
+use std::cmp;
+
+fn read_numbers() -> (u32, u32) {
+    (
+        read_user_input("Enter start page").parse().expect("Error parsing start page"),
+        read_user_input("Enter end page").parse().expect("Error parsing end page")
+    )
+}
+
+fn read_user_input(guess: &str) -> String {
+    println!("{}: ", guess); // add `?` if you care about errors here
+    let mut input = String::new();
+    match io::stdin().read_line(&mut input) {
+        Ok(_) => {
+            return input.trim().to_string();
+        },
+        Err(error) => {
+            println!("Reading error. {}", error);
+            process::exit(1)
+        }
+    }
+}
+
+fn read_dates() -> (NaiveDate, NaiveDate) {
+    let start_date_input = read_user_input("Enter start date");
+    let end_date_input = read_user_input("Enter end date");
+    let start_date;
+    let end_date;
+    match NaiveDate::parse_from_str(&start_date_input[..], "%Y-%m-%d") {
+        Ok(date) => start_date = date,
+        Err(error) => {
+            println!("Error on parsing start date. {}", error);
+            process::exit(1)
+        }
+    }
+    match NaiveDate::parse_from_str(&end_date_input[..], "%Y-%m-%d") {
+        Ok(date) => end_date = date,
+        Err(error) => {
+            println!("Error on parsing end date. {}", error);
+            process::exit(1)
+        }
+    }
+    return (start_date, end_date)
+}
+
+fn render_plan(start_page: u32, end_page: u32, start_date: NaiveDate, finish_date: NaiveDate) -> String {
+    let days = (finish_date - start_date).num_days() as u32 + 1;
+    let page_diff = end_page - start_page;
+    let page_per_day = (page_diff as f64 / days as f64).ceil() as u32;
+    let mut strings: Vec<String> = vec![];
+    for x in 0..days {
+        let last_page_for_day = cmp::min(end_page, start_page + (page_per_day * x) + page_per_day - 1);
+        strings.push(format!(
+            "Date: {}. Pages: {}-{}",
+            start_date + Duration::days(x.into()),
+            start_page + (page_per_day * x),
+            last_page_for_day,
+        ))
+    }
+    strings.join("\n")
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_render_plan() {
+        let got = render_plan(
+            1,
+            12,
+            NaiveDate::from_ymd_opt(2020, 1, 1).unwrap(),
+            NaiveDate::from_ymd_opt(2020, 1, 12).unwrap(),
+        );
+        assert_eq!(got, vec![
+            "Date: 2020-01-01. Pages: 1-1",
+            "Date: 2020-01-02. Pages: 2-2",
+            "Date: 2020-01-03. Pages: 3-3",
+            "Date: 2020-01-04. Pages: 4-4",
+            "Date: 2020-01-05. Pages: 5-5",
+            "Date: 2020-01-06. Pages: 6-6",
+            "Date: 2020-01-07. Pages: 7-7",
+            "Date: 2020-01-08. Pages: 8-8",
+            "Date: 2020-01-09. Pages: 9-9",
+            "Date: 2020-01-10. Pages: 10-10",
+            "Date: 2020-01-11. Pages: 11-11",
+            "Date: 2020-01-12. Pages: 12-12",
+        ].join("\n"));
+    }
+
+    #[test]
+    fn test_render_plan_2() {
+        let got = render_plan(
+            691,
+            767,
+            NaiveDate::from_ymd_opt(2023, 3, 27).unwrap(),
+            NaiveDate::from_ymd_opt(2023, 4, 2).unwrap(),
+        );
+        assert_eq!(got, vec![
+            "Date: 2023-03-27. Pages: 691-701",
+            "Date: 2023-03-28. Pages: 702-712",
+            "Date: 2023-03-29. Pages: 713-723",
+            "Date: 2023-03-30. Pages: 724-734",
+            "Date: 2023-03-31. Pages: 735-745",
+            "Date: 2023-04-01. Pages: 746-756",
+            "Date: 2023-04-02. Pages: 757-767",
+        ].join("\n"));
+    }
+}
+
+fn main() {
+    let (start_page, end_page) = read_numbers();
+    let (start_date, finish_date) = read_dates();
+    println!("{}", render_plan(start_page, end_page, start_date, finish_date))
+}
